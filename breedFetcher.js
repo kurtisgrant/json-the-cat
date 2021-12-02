f830b52ffd8fc6e1dddb724ac3f11209e6f446d0
Kurtis Grant <kurtis.b.grant@gmail.com>
2021-12-02 19:57:56 +0000

    Moved implementation into a function instead of doing everything in the global scope

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,17 +1,20 @@
 const request = require('request');
 
+const fetchBreed = (breed) => {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, (error, response, body) => {
+    if (error) {
+      return console.log(error);
+    }
+
+    const data = JSON.parse(body);
+
+    if (!data.length) {
+      return console.log('Sorry, no cat breeds by that name were found.');
+    } else {
+      console.log(data[0].description);
+    }
+  });
+};
+
 const requestedBreed = process.argv.slice(2)[0];
-
-request(`https://api.thecatapi.com/v1/breeds/search?q=${requestedBreed}`, (error, response, body) => {
-  if (error) {
-    return console.log(error);
-  }
-
-  const data = JSON.parse(body);
-
-  if (!data.length) {
-    return console.log('Sorry, no cat breeds by that name were found.');
-  } else {
-    console.log(data[0].description);
-  }
-});
+fetchBreed(requestedBreed);
